5d2b94679f4283abdb79758e79e8ab31ee0cad5a
MatthewRiente <40642715+MatthewRiente@users.noreply.github.com>
2019-01-08 16:30:00 -0500

    Completed challenge and helper

--- test/helpers_test.js
+++ test/helpers_test.js
@@ -6,7 +6,23 @@ describe('helpers', function() {
     describe('#getAllTodos', () => {
         it('function should print all todos', () => {
             // Well, this isn't a very good test. Make more! 
-            expect(helpers.getAllTodos(["plant seed", "make coffee"])).to.lengthOf(2)
+            expect(helpers.get()).to.lengthOf(2)
+        })
+    })
+    describe('#postNewTodo', () => {
+        it('function should add a new todo', () => {
+            expect(helpers.post("yeet")).to.have.lengthOf(3)
+        })
+    })
+    describe('#deleteTodoItem', () => {
+        it('function should delete a todo', () => {
+            expect(helpers.delete()).to.have.lengthOf(2)
+        })
+    })
+    describe('#updateTodoItem', () => {
+        it('function should update a todo', () => {
+            var array = helpers.get();
+            expect(helpers.update("seed", 0)).to.equal(array[0])
         })
     })
 })
