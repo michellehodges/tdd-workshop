2b3c2cc8e3e129e3b567c4213b2b641c1b2e1b3b
Seyfat Khamidov <seyfat123@gmail.com>
2019-01-09 08:56:21 -0500

    set up the routes and controllers

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+var helpers = require("./helpers");
 
-var helpers = require('./helpers')
-
-// var todos = ["plant seed", "make coffee"]
-
-app.get('/', function (req, res) {
-  res.send('Your thing is running!');
+app.get("/", function(req, res) {
+  res.send("Your thing is running!");
 });
 
-app.get('/api/todo', function (req, res) {
-  res.send(helpers.getAllTodos())
+app.get("/api/todo", function(req, res) {
+  res.send(helpers.getAllTodos());
 });
 
-app.get('/api/todo/:id', function (req, res) {
-  const id = req.params.id
-  let array = helpers.getAllTodos()
-  res.send(array[id])
-});
-
-app.post('/api/todo', (req, res) => {
-  helpers.addTodos(req.body)
-  res.send(helpers.getAllTodos())
-})
-
-app.put('/api/todo/:index', (req, res) => {
-  helpers.updateTodos(req.body, req.params.index)
+app.get("/api/todo/:index", function(req, res) {
+  const index = req.params.index;
   let array = helpers.getAllTodos();
-  res.send(array[id])
-})
+  res.send(array[index]);
+});
 
-app.delete('/api/todo/:index', (req, res) => {
-  helpers.deleteTodo(req.params.index)
-  res.send(helpers.getAllTodos())
-})
+app.post("/api/todo", (req, res) => {
+  const value = req.body.value;
+  helpers.addTodos(value);
+  res.send(helpers.getAllTodos());
+});
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...');
+app.put("/api/todo/:index", (req, res) => {
+  const value = req.body.value;
+  const index = req.params.index;
+  helpers.updateTodos(value, index);
+  let array = helpers.getAllTodos();
+  res.send(array);
+});
+
+app.delete("/api/todo/:index", (req, res) => {
+  const index = req.params.index;
+  helpers.deleteTodo(index);
+  res.send(helpers.getAllTodos());
+});
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000...");
 });
